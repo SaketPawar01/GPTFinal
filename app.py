1522edbece0a4a8e81eebfac0c8935dbef3fc35d
SaketPawar01 <102624704+SaketPawar01@users.noreply.github.com>
2024-12-01 07:41:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def generate():
         return f"An error occurred: {e}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Use PORT from environment or default to 5000
+    app.run(host='0.0.0.0', port=port)
